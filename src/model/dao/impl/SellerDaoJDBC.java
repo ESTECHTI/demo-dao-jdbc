9016ad6dae8db37be06283550af3b2cc9721e2de
ESTECHTI <e.s.tech@outlook.com>
2025-01-23 10:46:36 -0300

    SellerDaoJDBC and DaoFactory

---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,36 @@
+package model.dao.impl;
+
+import model.dao.SellerDao;
+
+import java.util.List;
+import model.entities.Seller;
+
+public class SellerDaoJDBC implements SellerDao {
+
+  @Override
+  public void insert(Seller obj) {
+    // Implementation here
+  }
+
+  @Override
+  public void update(Seller obj) {
+    // Implementation here
+  }
+
+  @Override
+  public void deleteById(Integer id) {
+    // Implementation here
+  }
+
+  @Override
+  public Seller findById(Integer id) {
+    // Implementation here
+    return null;
+  }
+
+  @Override
+  public List<Seller> findAll() {
+    // Implementation here
+    return null;
+  }
+}
